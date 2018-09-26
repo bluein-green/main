7d7761bc5e8b7964976f228c9705137f261a504c
bluein-green <j.minjia@gmail.com>
2018-09-26 10:42:58 +0800

    Ingredient.java: added ingredient class, modelled after Person class in AB4

--- src/main/java/seedu/address/model/person/Ingredient.java
+++ src/main/java/seedu/address/model/person/Ingredient.java
@@ -1,10 +1,53 @@
 package seedu.address.model.person;
 
+import java.util.Objects;
+
 /**
  * Represents an ingredient in the list of ingredients
  * Guarantees: TODO
  */
 public class Ingredient {
 
+    // Identity field
+    private final IngredientName name;
 
+    // Data fields
+    private IngredientQuantity quantity;
+
+    public Ingredient(IngredientName name, IngredientQuantity quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public IngredientName getName() {
+        return name;
+    }
+
+    public IngredientQuantity getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(IngredientQuantity quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (!(other instanceof Ingredient)) {
+            return false;
+        }
+
+        Ingredient otherIngredient = (Ingredient) other;
+        return otherIngredient.getName().equals(getName())
+                && otherIngredient.getQuantity() == getQuantity();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, quantity);
+    }
 }
