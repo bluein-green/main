21c8eeabe28659dada059deb3a7d14e8cc07ed32
bluein-green <j.minjia@gmail.com>
2018-09-26 10:17:00 +0800

    IngredientQuantity.java: added ingredient quantity class

---
+++ src/main/java/seedu/address/model/person/IngredientQuantity.java
@@ -0,0 +1,52 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+
+/**
+ * Represents an ingredient's quantity in the list of ingredients
+ * Guarantees: TODO
+ */
+public class IngredientQuantity {
+        public static final String MESSAGE_QUANTITY_CONSTRAINTS =
+            "Quantity should only contain numbers";
+    public static final String QUANTITY_VALIDATION_REGEX = "\\d+";
+    public final int quantity;
+
+    /**
+     * Constructs a {@code Phone}.
+     *
+     * @param quantity A valid quantity value expressed as a string.
+     */
+    public IngredientQuantity(String quantity) {
+        requireNonNull(quantity);
+        checkArgument(isValidPhone(quantity), MESSAGE_QUANTITY_CONSTRAINTS);
+        this.quantity = Integer.parseInt(quantity);
+    }
+
+    /**
+     * Returns true if a given string is a valid phone number.
+     */
+    public static boolean isValidPhone(String test) {
+        return test.matches(QUANTITY_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(quantity);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof IngredientQuantity // instanceof handles nulls
+                && quantity == (((IngredientQuantity) other).quantity)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return quantity; // primitive int is its own hash
+    }
+
+}
