786259212904d31e4d7a0a7b920daf1c58589691
bluein-green <j.minjia@gmail.com>
2018-11-10 23:14:51 +0800

    AnalysisManagerTest edited and Predicate, TypicalTransactions

--- src/test/seedu/address/analysis/AnalysisManagerTest.java
+++ src/test/seedu/address/analysis/AnalysisManagerTest.java
@@ -1,4 +1,30 @@
 package seedu.address.analysis;
 
+import static seedu.address.testutil.transaction.TypicalTransactions.getTypicalFilteredTransactions;
+import static seedu.address.testutil.transaction.TypicalTransactions.getTypicalTransactionList;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class AnalysisManagerTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private AnalysisManager analysisManager = new AnalysisManager(getTypicalTransactionList(),
+            getTypicalFilteredTransactions());
+
+
+    // analyseProfit
+    // analyseCost
+    // analyseRevenue
+    // calculateTotalProfit
+    // calculateTotalCost
+    // calculateTotalRevenue
+
+
+    @Test
+    public void calculateTotalProfit() {
+        //analysisManager.analyseProfit()
+    }
 }
--- src/test/seedu/address/analysis/TransactionInADayPredicateTest.java
+++ src/test/seedu/address/analysis/TransactionInADayPredicateTest.java
@@ -1,10 +1,16 @@
 package seedu.address.analysis;
 
 import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import org.junit.Test;
 
+import seedu.address.model.drink.Date;
+import seedu.address.model.drink.Drink;
+import seedu.address.model.drink.Price;
+import seedu.address.model.drink.Quantity;
 import seedu.address.model.transaction.Transaction;
+import seedu.address.model.transaction.TransactionType;
 import seedu.address.testutil.transaction.TransactionBuilder;
 
 public class TransactionInADayPredicateTest {
@@ -20,4 +26,34 @@ public class TransactionInADayPredicateTest {
     }
 
     // TODO: test for failure, but with Transaction initialising date to current date, it is not possible to test
+//    @Test
+//    public void test_isNotWithinDay_returnsFalse() {
+//        TransactionInDayPredicate predicate = new TransactionInDayPredicate();
+//        Transaction transactionToday = new TransactionBuilder().withDrink("Pepsi").withDateToday()
+//                .withTransactionType("sale")
+//                .withAmountMoney("5.00")
+//                .withQuantity("40")
+//                .build();
+//    }
+//
+//
+//    private class TransactionStub extends Transaction {
+//        private TransactionType transactionType;
+//        private Date transactionDate;
+//        private Drink drinkTransacted;
+//        private Quantity quantityTransacted;
+//        private Price amountMoney;
+//        private long id;
+//
+//        public TransactionStub(TransactionType transactionType, Drink drinkTransacted,
+//                               Quantity quantityTransacted, Price amountMoney, Date date) {
+//            requireAllNonNull(transactionType, drinkTransacted, quantityTransacted, amountMoney);
+//            this.transactionType = transactionType;
+//            this.drinkTransacted = drinkTransacted;
+//            this.quantityTransacted = quantityTransacted;
+//            this.amountMoney = amountMoney;
+//            this.transactionDate = date;
+//            id = new java.util.Date().getTime();
+//        }
+//    }
 }
