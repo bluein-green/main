c87039b73295f4f34f85dda3e2bbaa5e64815c62
bluein-green <j.minjia@gmail.com>
2018-11-10 17:51:24 +0800

    AnalysisPeriodType: edited to use correct predicates

--- src/main/java/seedu/address/analysis/AnalysisPeriodType.java
+++ src/main/java/seedu/address/analysis/AnalysisPeriodType.java
@@ -25,14 +25,14 @@ public enum AnalysisPeriodType {
     WEEK() {
         @Override
         public Predicate<Transaction> getPeriodFilterPredicate() {
-            return new TransactionInDayPredicate();
+            return new TransactionInSevenDaysPredicate();
         }
     },
 
     MONTH {
         @Override
         public Predicate<Transaction> getPeriodFilterPredicate() {
-            return new TransactionInDayPredicate();
+            return new TransactionInThirtyDaysPredicate();
         }
     };
 
@@ -40,35 +40,5 @@ public enum AnalysisPeriodType {
     public static final String WRONG_PERIOD_MESSAGE = "You have entered an invalid period";
 
     public abstract Predicate<Transaction> getPeriodFilterPredicate();
-
-    public static AnalysisPeriodType getPeriod(String test) throws ParseException {
-        test = test.replaceAll("\\s+", "");
-        if (test.equals(HYPHEN_DAY)) {
-            return DAY;
-        }
-        if (test.equals(HYPHEN_WEEK)) {
-            return WEEK;
-        }
-        if (test.equals(HYPHEN_MONTH)) {
-            return MONTH;
-        }
-        throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, WRONG_PERIOD_MESSAGE));
-    }
-
-
-    /**
-     * Returns true if the argument entered is valid, as defined in {@code CliSyntax}
-     */
-    public static boolean isValidPeriod(String test) {
-        test = test.replaceAll("\\s+", "");
-
-        switch (test) {
-        case HYPHEN_DAY: case HYPHEN_WEEK: case HYPHEN_MONTH:
-            return true;
-
-        default:
-            return false;
-        }
-    }
 
 }
